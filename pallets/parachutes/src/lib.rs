9af1095c9c1a2d6f4412e63709ac62939b4a2276
ChubbySkullFarm <contact@chubbyskull.farm>
2025-02-15 14:13:01 -0700

    Update lib.rs

--- pallets/parachutes/src/lib.rs
+++ pallets/parachutes/src/lib.rs
@@ -1,8 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 //TODO:
-// WEIGHTS
-// BENCHMARKING
 // append() instead of push()?
 // Work on properly defining types with traits
 //    - Move to separate module
